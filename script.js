988c6c9b19d79edada6fdcddf270681c3ef51148
jmatdawg <matthews_jakiera@yahoo.com>
2018-01-26 16:53:51 -0600

    still not finished

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+
+var sentences = ["ten ate neite ate nee enet ite ate inet ent eate", 
+"Too ato too nOt enot one totA not anot tOO aNot", 
+"oat itain oat tainnate eate tea anne inant nean", 
+"itant eate anot eat nato inate eatanot tain eat", 
+"nee ene ate ite tent tiet ent ine ene ete ene ate"];
+
+
+let sentenceContainer = $('#sentence');
+let sentenceCounter = 0;
+
+let splitSentence = sentences.map((sentence) => {
+    return sentence.split(" ");
+})
+const configuredSentence = splitSentence.map((character, index) => {
+    return `<span id="target-${index}">${character}</span>`;
+}).join('');
+
+sentenceContainer.append(sentences);
+
 $(document).ready(() => {
     $('#keyboard-upper-container').hide();
     })
@@ -31,6 +51,9 @@ $(document).bind("keydown", (e) => {
 $(document).bind("keyup", (e) => {
     $('#32' + (e.keyCode)).css("background-color", "");
 })
+
+
+
 
 
 //create a sentence counter to switch out sentence
